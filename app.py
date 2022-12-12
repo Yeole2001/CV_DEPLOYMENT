dad7582af19ee0e08cb64c4d82656e6a0f7c0c38
Atharv Yeole <54111500+Yeole2001@users.noreply.github.com>
2022-12-12 23:34:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,9 +54,11 @@ else:
     bw = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
     img = cv2.bitwise_not(bw_img)
     
+    st.write("Input Image...")
     st.image(imgcp, width=500)
     st.write("After Preprocessing...")
     st.image(img, width=500)
+    st.write("Prediction...")
     
 
 
